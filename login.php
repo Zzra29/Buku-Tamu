f1d5e1b49e0d2a30cfb5b85cff0145cb3c1216b9
Zulfani Z <kajubbaju@gmail.com>
2026-09-24 05:51:24 +0700

    selesai

--- login.php
+++ login.php
@@ -1,58 +1,40 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 session_start();
-?>
 
-<?php
+// cek bila ada user yang sudah login maka akan redirect ke halaman dashboard
+if (isset($_SESSION['login'])) {
+    header('Location: index.php');
+    exit;
+}
+
 require 'koneksi.php';
+
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     $result = mysqli_query($koneksi, "SELECT * FROM users WHERE username = '$username'");
 
-    // cek apakah ada username
     if (mysqli_num_rows($result) == 1) {
-
-        // cek apakah passwordnya benar
         $row = mysqli_fetch_assoc($result);
 
         if (password_verify($password, $row['password'])) {
-    // set session
-    $_SESSION['login']    = true;
-    $_SESSION['username'] = $username;
-    $_SESSION['role']     = $row['user_role'];
+            $_SESSION['login']    = true;
+            $_SESSION['username'] = $username;
+            $_SESSION['role']     = $row['user_role'];
 
-    // login berhasil
-    header("Location: index.php");
-    exit;
-}
+            header("Location: index.php");
+            exit;
+        }
     }
 
     $error = true;
 }
-
 ?>
-
-<body class="bg-gradient-primary">
-
-<div class="container">
-
-<?php
-if (isset($error)) : ?>
-<div class="alert alert-danger mt-3" role="alert">
-    Username atau password salah!
-</div>
-<?php
-endif;
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -61,31 +43,33 @@ endif;
 
     <title>SB Admin 2 - Login</title>
 
-    <!-- Custom fonts for this template-->
     <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
 <body class="bg-gradient-primary">
 
     <div class="container">
 
+        <?php if (isset($error)) : ?>
+        <div class="alert alert-danger mt-3" role="alert">
+            Username atau password salah!
+        </div>
+        <?php endif; ?>
+
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-xl-10 col-lg-12 col-md-9">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-none d-lg-flex align-items-center justify-content-center">
+    <img src="assets/img/undraw_posting_photo.svg"
+         alt="Login"
+         class="img-fluid"
+         style="max-width: 90%; height: auto;">
+</div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -122,23 +106,15 @@ endif;
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
 
     </div>
 
-    <!-- Bootstrap core JavaScript-->
     <script src="assets/vendor/jquery/jquery.min.js"></script>
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
     <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="assets/js/sb-admin-2.min.js"></script>
 
 </body>
-
 </html>
